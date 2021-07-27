5da63b5eac39fc459c07c68c9731a2b9eaa2c0e6
RobsonAndradev <robsom.s.a@gmail.com>
2021-07-27 15:10:50 -0300

    Add more options to find mix file

--- get_mix_version.py
+++ get_mix_version.py
@@ -1,3 +1,5 @@
+import sys
+
 def main():
     line = get_version_line()
     version = grep_version(line)
@@ -7,7 +9,8 @@ def main():
 def get_version_line():
     lines = []
 
-    with open("./mix.exs", "r") as f:
+    mix_file = get_mix_file()
+    with open(mix_file, "r") as f:
         lines = f.readlines()
 
     for line in lines:
@@ -15,6 +18,12 @@ def get_version_line():
             return line
 
     return "0.0.1"
+
+def get_mix_file():
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+
+    return "./mix.exs"
 
 def grep_version(line):
     first = ': "'
